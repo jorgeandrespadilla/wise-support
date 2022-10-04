9bc0bd16b6e7660054ccfdbeaa7b65e7b103aa94
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-10-04 12:29:10 -0500

    feat(client): configure project styles using Tailwind CSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: colors.blue[500],
+        neutral: colors.gray[500],
+      },
+    },
+  },
+  plugins: [],
+}
