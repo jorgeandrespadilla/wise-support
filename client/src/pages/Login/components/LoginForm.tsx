eb11393ed460d3ed8e4ceb85cbe89e957c09a66e
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-11-21 16:12:33 -0500

    refactor(client): update login page HTTP request handling

--- client/src/pages/Login/components/LoginForm.tsx
+++ client/src/pages/Login/components/LoginForm.tsx
@@ -1,35 +1,59 @@
+import { useMutation } from "@tanstack/react-query";
 import Button from "components/Button";
 import Card from "components/Card";
 import { PasswordField, TextField } from "components/Form";
-import { useAuth } from "hooks";
+import { useAuth, useLoadingToast } from "hooks";
 import { useForm } from "react-hook-form";
 import { useLocation, useNavigate } from "react-router-dom";
 import { authenticateUser } from "services/authentication";
 import { LoginRequest } from "types";
 import { handleAPIError } from "utils/validation";
 
-type FormData = LoginRequest;
+type FormData = {
+    email: string;
+    password: string;
+}
 
 function LoginForm() {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
     const { control, handleSubmit, ...form } = useForm<FormData>({
         defaultValues: {
             email: "",
             password: "",
         },
     });
-    const { isAuthenticated, login } = useAuth();
-    const navigate = useNavigate();
-    const location = useLocation();
 
-    const handleLogin = (user: FormData) => {
-        const targetPath = location.state ? location.state.pathname : "/";
-        authenticateUser(user).then(data => {
-            login(data.authToken);
-            navigate(targetPath);
-        }).catch((err) => {
-            handleAPIError(err, { form });
-        });
-    };
+    const { isAuthenticated, login } = useAuth();
+
+    const loginToast = useLoadingToast("login", {
+        loading: "Validando credenciales...",
+    });
+    const { mutate: handleLogin } = useMutation(
+        async (credentials: FormData) => {
+            loginToast.loading();
+            const request: LoginRequest = {
+                email: credentials.email,
+                password: credentials.password,
+            };
+            return await authenticateUser(request);
+        },
+        {
+            onSuccess: (data) => {
+                loginToast.success();
+                const targetPath = location.state ? location.state.pathname : "/";
+                login(data.authToken);
+                navigate(targetPath);
+            },
+            onError: (e) => {
+                loginToast.error();
+                handleAPIError(e, { form, toastId: loginToast.toastId });
+            },
+
+        },
+    );
 
     if (isAuthenticated) {
         navigate("/");
