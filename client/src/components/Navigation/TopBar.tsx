e7f1396511b2d8dfc9cc87367ef4dfe839a61374
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-12-18 15:32:29 -0500

    feat(client): update main layout and navigation bar

--- client/src/components/Navigation/TopBar.tsx
+++ client/src/components/Navigation/TopBar.tsx
@@ -1,23 +1,25 @@
+import { NavLink } from "react-router-dom";
 import { ArrowLeftOnRectangleIcon, ChevronDownIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Authorize from "components/Authorize";
 import Avatar from "components/Avatar";
 import DropdownMenu from "components/DropdownMenu";
-import { useAuth } from "hooks/useAuth";
-import { useCurrentUser } from "hooks/useCurrentUser";
-import { NavLink } from "react-router-dom";
-import { DropdownMenuOption, LinkConfig } from "types/ui";
 import NavItem from "./NavItem";
+import { useAuth, useTheme, useCurrentUser } from "hooks";
+import { DropdownMenuOption, LinkConfig } from "types/ui";
+import logoLight from 'assets/logo-light.svg';
+import logoDark from 'assets/logo-dark.svg';
 
 type NavigationProps = {
-  title: string;
+  appTitle: string;
   links: LinkConfig[];
 };
 
 function TopBar({
-  title = "",
+  appTitle = "",
   links = []
 }: NavigationProps) {
   const { syncLogout } = useAuth();
+  const { isDarkTheme } = useTheme();
   const { user } = useCurrentUser();
 
   const dropdownMenuOptions: DropdownMenuOption[][] = [
@@ -26,7 +28,7 @@ function TopBar({
         label: "Perfil",
         icon: <UserCircleIcon className="h-5 w-5" />,
         navigateTo: "/profile",
-      }
+      },
     ],
     [
       {
@@ -38,10 +40,14 @@ function TopBar({
   ];
 
   return (
-    <header className="flex flex-row justify-between items-center border-b-2 border-gray-200 px-8 py-4">
+    <header className="flex flex-row justify-between items-center dark:bg-slate-800 border-b-2 border-gray-200 dark:border-gray-700 px-8 py-4">
       <div className="flex flex-row items-center space-x-10">
         <NavLink to="/">
-          <h1 className="font-bold text-xl text-blue-500">{title}</h1>
+          {
+            isDarkTheme
+              ? <img src={logoDark} alt={appTitle} title={appTitle} className="h-10" />
+              : <img src={logoLight} alt={appTitle} title={appTitle} className="h-10" />
+          }
         </NavLink>
         <ul className="flex flex-row space-x-2">
           {
@@ -53,13 +59,13 @@ function TopBar({
           }
         </ul>
       </div>
-      <DropdownMenu 
+      <DropdownMenu
         toggle={
           <div className="flex flex-row items-center space-x-2 cursor-pointer text-gray-600">
             <Avatar userName={user?.fullName ?? ""} />
             <ChevronDownIcon className="w-5 h-5" />
           </div>
-        } 
+        }
         optionGroups={dropdownMenuOptions} />
     </header>
   );
