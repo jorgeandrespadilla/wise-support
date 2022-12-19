dff430574200e119df2262c5e59ac224f3301939
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-12-18 23:44:44 -0500

    feat(client): add responsive navigation

--- client/src/components/Navigation/TopBar/components/NavItem.tsx
+++ client/src/components/Navigation/TopBar/components/NavItem.tsx
@@ -5,21 +5,30 @@ type NavItemProps = {
   label: string;
   icon?: React.ReactNode;
   index: number;
+  size?: "md" | "lg";
+  onClick?: () => void;
+};
+
+export const itemSize = {
+  md: 'px-4 py-2 text-base',
+  lg: 'px-6 py-3 text-md',
 };
 
 function NavItem({
   to,
   label,
   icon,
-  index
+  index,
+  size = "md",
+  onClick = () => {},
 }: NavItemProps) {
   return (
     <li>
       <NavLink className={
         ({ isActive }: { isActive: boolean }) => {
-          return `block hover:bg-blue-100 dark:hover:bg-opacity-10 px-4 py-2 rounded transition-all duration-300 ${isActive ? "text-blue-500" : "text-gray-500 dark:text-gray-200"}`;
+          return `block hover:bg-blue-100 dark:hover:bg-opacity-10 rounded transition-all duration-300 ${itemSize[size]} ${isActive ? "text-blue-500" : "text-gray-500 dark:text-gray-200"}`;
         }
-      } key={index} to={to}>
+      } key={index} to={to} onClick={onClick}>
         <div className="flex flex-row gap-2 items-center">
           {icon}
           {label}
