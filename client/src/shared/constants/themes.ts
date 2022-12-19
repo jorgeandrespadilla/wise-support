585e205a12a4b87fc12d6309b40c10cf1be1f1ed
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2022-12-18 22:04:43 -0500

    feat(client): add responsive utilities

--- client/src/shared/constants/themes.ts
+++ client/src/shared/constants/themes.ts
@@ -1,3 +1,16 @@
-import { Theme } from "types/ui";
+import { Theme, ThemeBreakpoint } from "types/ui";
 
 export const allThemes: Theme[] = ["light", "dark", "system"];
+
+export const sizeByBreakpoint: Record<ThemeBreakpoint, number> = {
+    'sm': 640,
+    'md': 768,
+    'lg': 1024,
+    'xl': 1280,
+};
+
+export const breakpoints = {
+    up: (breakpoint: ThemeBreakpoint) => `(min-width: ${sizeByBreakpoint[breakpoint]}px)`,
+    down: (breakpoint: ThemeBreakpoint) => `(max-width: ${sizeByBreakpoint[breakpoint]}px)`,
+    between: (start: ThemeBreakpoint, end: ThemeBreakpoint) => `(min-width: ${sizeByBreakpoint[start]}px) and (max-width: ${sizeByBreakpoint[end] - 0.02}px)`,
+};
